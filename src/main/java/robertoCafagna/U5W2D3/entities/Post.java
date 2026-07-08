584a5b2740fea80be6fa0ed51ac48402c0ff141e
cafagnarob <cafagna.rob@gmail.com>
2026-07-08 14:22:49 +0200

    first

---
+++ src/main/java/robertoCafagna/U5W2D3/entities/Post.java
@@ -0,0 +1,53 @@
+package robertoCafagna.U5W2D3.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@NoArgsConstructor
+@Entity
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String categoria;
+
+    @Column(nullable = false)
+    @Setter
+    private String titolo;
+
+    @Column(nullable = false)
+    @Setter
+    private String cover;
+
+    @Column(nullable = false)
+    @Setter
+    private String contenuto;
+
+    @Column(nullable = false)
+    @Setter
+    private Integer minutiDiLettura;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+
+    public Post(String categoria, String titolo, String contenuto, Integer minutiDiLettura, User user) {
+
+        this.categoria = categoria;
+        this.titolo = titolo;
+        this.cover = "https://picsum.photos/200/300";
+        this.contenuto = contenuto;
+        this.minutiDiLettura = minutiDiLettura;
+        this.user = user;
+    }
+
+
+}
---
+++ src/main/java/robertoCafagna/U5W2D3/entities/User.java
@@ -0,0 +1,46 @@
+package robertoCafagna.U5W2D3.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@ToString
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String cognome;
+
+    @Column(nullable = false, unique = true)
+    @Setter
+    private String email;
+
+    @Column(nullable = false)
+    private LocalDate dataDiNascita;
+
+    @Column(nullable = false)
+    @Setter
+    private String avatar;
+
+
+    public User(String nome, String cognome, String email, LocalDate dataDiNascita) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.email = email;
+        this.dataDiNascita = dataDiNascita;
+        this.avatar = "https://ui-avatars.com/api/?name=Mario+Rossi";
+    }
+
+}
